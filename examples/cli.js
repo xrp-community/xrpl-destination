1417a459365caa2b01348aaac1cf4ba6d35dd7e8
Wietse Wind <mail@wietse.com>
2019-06-20 15:04:56 +0200

    Updated samples

--- examples/cli.js
+++ examples/cli.js
@@ -1,32 +1,33 @@
 const XRPLDestination = require('../src')
 
-console.log()
-console.log()
+console.log(`\n`)
+
+const account = 'rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpf'
 
 console.log(
-  JSON.stringify(new XRPLDestination('rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDY'), null, 2),
-  JSON.stringify(new XRPLDestination('rpEV8JKNQpRdeTop5quDYMMA5ynxJLrUWpjbQvzzkQytLc5'), null, 2),
-  JSON.stringify(new XRPLDestination('rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDYVaCSRUkH8'), null, 2)
+  JSON.stringify(new XRPLDestination(account), null, 2),
+  JSON.stringify(new XRPLDestination('r1WTvVjuoBM9vsm2p395AyzCQcJyEp8aG4YHcqE3XLDehK'), null, 2),
+  JSON.stringify(new XRPLDestination('rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpfTbxXR2tgs'), null, 2),
+  JSON.stringify(new XRPLDestination('XfHcYHS0rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpf'), null, 2)
 )
 
-console.log()
-console.log()
+console.log(`\n-------\n`)
 
 console.log(
-  JSON.stringify(new XRPLDestination('rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDY:0'), null, 2),
-  JSON.stringify(new XRPLDestination('rpEV8JKNQpRdeTop5quDYMMA5ynxJLap6KUDaXQA2L9LPh8'), null, 2),
-  JSON.stringify(new XRPLDestination('rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDYT3KnHMu1D4'), null, 2)
+  JSON.stringify(new XRPLDestination(`${account}:0`), null, 2),
+  JSON.stringify(new XRPLDestination('r1WTvVjuoBM9vsm2p395AyzCQcJyEfcHYq9yDEBxVaZPrv'), null, 2),
+  JSON.stringify(new XRPLDestination('rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpfRaqV96w6Bi'), null, 2),
+  JSON.stringify(new XRPLDestination('XsjB8w300rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpf'), null, 2)
 )
 
-console.log()
-console.log()
+console.log(`\n-------\n`)
 
 
 console.log(
-  JSON.stringify(new XRPLDestination('rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDY:13371337'), null, 2),
-  JSON.stringify(new XRPLDestination('rpEV8JKNQpRdeTop5quDYMMA5ynxJL9FjsZTcy7TEsu4eb5'), null, 2),
-  JSON.stringify(new XRPLDestination('rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDYVW57KBSpCcZagTqMxyVS'), null, 2)
+  JSON.stringify(new XRPLDestination(`${account}:13371337`), null, 2),
+  JSON.stringify(new XRPLDestination('r1WTvVjuoBM9vsm2p395AyzCQcJyEBQKHUQVCJ6dDGbYU7'), null, 2),
+  JSON.stringify(new XRPLDestination('rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpf7vSBPCZR1h1tmgqHTkEp'), null, 2),
+  JSON.stringify(new XRPLDestination('XatLo2R013371337rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpf'), null, 2)
 )
 
-console.log()
-console.log()
+console.log(`\n`)
